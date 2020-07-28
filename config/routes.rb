47d067e6425c1dfcac19ce58fdd5119002cf1770
falahaboud <falah.aboud@gmail.com>
2020-07-28 10:27:51 +0200

    login, noty, und edit wurde gemacht

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'pages/home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+    
+  devise_for :users, 
+  path: '', 
+  path_names: {sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout'},
+  controllers: {registrations: 'registrations'}
 end
