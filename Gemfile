843805d8f518ae51c8ede2d8f6db16169247d377
falahaboud <falah.aboud@gmail.com>
2020-07-30 09:26:39 +0200

    Dashboard ist ok

--- Gemfile
+++ Gemfile
@@ -31,7 +31,9 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'bulma-rails', '~> 0.9.0'
 gem 'bulma-extensions-rails', '~> 6.2.7'
-gem 'devise', '=4.7.2'
+gem 'devise', '= 4.7.2'
+
+gem 'omniauth', '= 1.9.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get '/dashboard', to: 'users#dashboard'
+
+  post 'users/edit', to:'users#update'
+
   root 'pages#home'
     
   devise_for :users, 
   path: '', 
   path_names: {sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout'},
-  controllers: {registrations: 'registrations'}
+  controllers: {omniauth_callbacks: 'omiauth_callbacks', registrations: 'registrations'}
 end
